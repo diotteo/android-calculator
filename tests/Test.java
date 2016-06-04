d9c889078bdfa3b5508eab3cfadf9031b399c811
Olivier Diotte <olivier@diotte.ca>
2016-06-04 13:46:30 -0400

    Test: change expression list

--- tests/Test.java
+++ tests/Test.java
@@ -6,18 +6,18 @@ import ca.dioo.android.dioo_calc.ExpressionTree;
 public class Test {
 	public static void main(String[] args) {
 		List<String> exprList;
-		if (false) {
+		if (true) {
 			exprList = Arrays.asList(
 					//"-20..+30**(45.2+*.5E-2)-36",
 					"2**(2)",
 					"200.+2",
 					"1+2*3+5/4",
 					"1+2*3/4-5+6",
+					"0xff ** -(1.)",
 					"-20+30*(45.2+-.5E-2)-36",
 					"-20+30**(45.2+-.5E-2)-36");
 		} else {
-			exprList = Arrays.asList(
-					"2-(-2)");
+			exprList = Arrays.asList();
 		}
 
 		ExpressionTree.DEBUG = true;
