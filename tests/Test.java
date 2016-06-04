12c0c8f09f4df17ed39b9c13497c6ab12ae5aee8
Olivier Diotte <olivier@diotte.ca>
2016-06-04 05:24:30 -0400

    add tests files

---
+++ tests/Test.java
@@ -0,0 +1,51 @@
+import java.util.List;
+import java.util.Arrays;
+
+import ca.dioo.android.dioo_calc.ExpressionTree;
+
+public class Test {
+	public static void main(String[] args) {
+		List<String> exprList;
+		if (false) {
+			exprList = Arrays.asList(
+					//"-20..+30**(45.2+*.5E-2)-36",
+					"2**(2)",
+					"200.+2",
+					"1+2*3+5/4",
+					"1+2*3/4-5+6",
+					"-20+30*(45.2+-.5E-2)-36",
+					"-20+30**(45.2+-.5E-2)-36");
+		} else {
+			exprList = Arrays.asList(
+					"2-(-2)");
+		}
+
+		ExpressionTree.DEBUG = true;
+
+		for (String expr: exprList) {
+			System.out.println("expr:" + expr);
+			List<ExpressionTree.Token> tokList = ExpressionTree.parse(expr);
+			printList(tokList);
+			System.out.println("");
+
+			List<ExpressionTree.Token> optList = ExpressionTree.compressList(tokList);
+			printList(optList);
+			System.out.println("");
+
+			ExpressionTree tree = ExpressionTree.buildTree(optList);
+			tree.printTree();
+			System.out.println("\nresult:" + tree.getResult());
+
+			System.out.println("\n");
+		}
+	}
+
+
+	public static void printList(List<ExpressionTree.Token> l) {
+		String sep = "  ";
+		for (ExpressionTree.Token t: l) {
+			System.out.print(sep + t.toString());
+			sep = ", ";
+		}
+	}
+}
