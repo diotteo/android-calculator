1a23ea6b210312d82084325b224c5ea76f2f276a
Olivier Diotte <olivier@diotte.ca>
2017-06-17 15:19:20 -0400

    Use custom Keyboard

--- src/ca/dioo/android/MainActivity.java
+++ src/ca/dioo/android/MainActivity.java
@@ -8,12 +8,18 @@ import android.view.View;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.content.Context;
+import android.text.Editable;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ScrollView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.view.inputmethod.InputMethodManager;
+import android.view.WindowManager;
+import android.view.View.OnFocusChangeListener;
 
 import android.widget.ImageButton;
 import android.view.LayoutInflater;
@@ -24,6 +30,7 @@ public class MainActivity extends Activity {
 	private LinearLayout mResultView;
 	private EditText mInputView;
 	private boolean mEmptyResults;
+	private KeyboardView mKbdView;
 
 
 	private static class ExpressionWidget extends LinearLayout {
@@ -39,9 +46,26 @@ public class MainActivity extends Activity {
 	}
 
 
+	public void hideCustomKeyboard() {
+		mKbdView.setVisibility(View.GONE);
+		mKbdView.setEnabled(false);
+	}
+
+	public void showCustomKeyboard(View v) {
+		mKbdView.setVisibility(View.VISIBLE);
+		mKbdView.setEnabled(true);
+		if (v != null) {
+			((InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(v.getWindowToken(), 0);
+		}
+	}
+
+	public boolean isCustomKeyboardVisible() {
+		return mKbdView.getVisibility() == View.VISIBLE;
+	}
+
+
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
 
@@ -49,6 +73,83 @@ public class MainActivity extends Activity {
 		mResultView = (LinearLayout) findViewById(R.id.result_view);
 		mInputView = (EditText) findViewById(R.id.input_view);
 		mEmptyResults = true;
+
+		Keyboard mKbd = new Keyboard(this, R.xml.keyboard);
+		mKbdView = (KeyboardView) findViewById(R.id.keyboardview);
+		mKbdView.setKeyboard(mKbd);
+		mKbdView.setPreviewEnabled(false);
+
+		mKbdView.setOnKeyboardActionListener(new KeyboardView.OnKeyboardActionListener() {
+				private static final int KEYCODE_CLEAR = -100;
+				private static final int KEYCODE_ENTER = -101;
+				private static final int KEYCODE_CANCEL = Keyboard.KEYCODE_CANCEL;
+				private static final int KEYCODE_DELETE = Keyboard.KEYCODE_DELETE;
+
+				@Override
+				public void onKey(int primaryCode, int[] keyCodes) {
+					View focusCurrent = MainActivity.this.getWindow().getCurrentFocus();
+					if (focusCurrent == null || focusCurrent.getClass() != EditText.class) {
+						return;
+					}
+
+					EditText edittext = (EditText) focusCurrent;
+					int start = edittext.getSelectionStart();
+					Editable editable = edittext.getText();
+
+					if (primaryCode == KEYCODE_CANCEL) {
+						hideCustomKeyboard();
+					} else if (primaryCode == KEYCODE_DELETE) {
+						if (editable != null && start > 0) {
+							editable.delete(start - 1, start);
+						}
+					} else if (primaryCode == KEYCODE_ENTER) {
+						displayResult(focusCurrent);
+					} else if (primaryCode == KEYCODE_CLEAR) {
+						clearInput(focusCurrent);
+					} else {
+						editable.insert(start, Character.toString((char) primaryCode));
+					}
+				}
+
+				@Override public void onPress(int arg0) {
+				}
+
+				@Override public void onRelease(int primaryCode) {
+				}
+
+				@Override public void onText(CharSequence text) {
+				}
+
+				@Override public void swipeDown() {
+				}
+
+				@Override public void swipeLeft() {
+				}
+
+				@Override public void swipeRight() {
+				}
+
+				@Override public void swipeUp() {
+				}
+				});
+
+		mInputView.setOnFocusChangeListener(new OnFocusChangeListener() {
+				@Override
+				public void onFocusChange(View v, boolean hasFocus) {
+					if (hasFocus) {
+						showCustomKeyboard(v);
+					} else {
+						hideCustomKeyboard();
+					}
+				}
+				});
+		mInputView.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					showCustomKeyboard(v);
+				}
+				});
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 	}
 
 
@@ -69,7 +170,7 @@ public class MainActivity extends Activity {
 				Toast.makeText(this, "Invalid expression", Toast.LENGTH_SHORT).show();
 				return;
 			}
-			mInputView.setText("");
+			clearInput(null);
 
 			String prefix = "\n";
 			if (mEmptyResults) {
